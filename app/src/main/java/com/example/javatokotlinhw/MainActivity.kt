0f22b54a7c253a60823a0b18740b58c0b536f339
warcerg <warcerg@gmail.com>
2021-08-09 21:56:03 +0300

    Task 3 set on click listener for button

--- app/src/main/java/com/example/javatokotlinhw/MainActivity.kt
+++ app/src/main/java/com/example/javatokotlinhw/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.javatokotlinhw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.javatokotlinhw.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initButton()
+    }
+
+    private fun initButton() {
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                //to update text views
+            }
+
+        })
     }
 }
